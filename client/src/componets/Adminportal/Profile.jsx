9ec6516cf191f574a19767ae3d38f0034c3fe313
keyur6021 <86195043+keyur6021@users.noreply.github.com>
2022-02-18 12:54:57 +0530

    contionue with profil

--- client/src/componets/Adminportal/Profile.jsx
+++ client/src/componets/Adminportal/Profile.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
+import {ListGroupItem,ListGroup,Card}  from 'react-bootstrap'
 import profile from '../../image/profile.jpg'
-import { AiFillCaretRight } from "react-icons/ai";
+import {MdOutlineDriveFileRenameOutline,MdMarkEmailUnread} from "react-icons/md";
+import {BsTelephoneForwardFill} from 'react-icons/bs'
+import {RiAdminFill} from 'react-icons/ri'
 import axios from 'axios';
-import pic from '../../image/background.png'
-import { Card, CardContent } from '@mui/material';
+import background from '../../image/background.jpg'
 
 export const Profile = () => {
 
@@ -24,34 +26,103 @@ export const Profile = () => {
 
     return (
         <>
+        <div className="background">
+    
+<Card style={{ width: '78%',marginLeft:"285px",height:'580px'}}>
 
-            <div className="row">
-                <div style={{marginLeft:"150px",fontWeight:"bold",color:"green",fontFamily:"'Shizuru', 'cursive'"}} className="heading text-center">
-                <h1>Profile Details :-</h1>
-                </div><hr/>
-                <div className="col-2">
-                    
-                </div>
-                <div className="col-4">
+  <Card.Body>
+      <div className="row">
+          <div className="col-2">
+          <img style={{ borderRadius: "88px", width: "170px", height: "170px", marginLeft: "15px"}}
+        src={user.pic} alt="profile" />
+          </div>
+          <div className="col-4"></div>
+          <div className="col-6"></div>
+      </div>
 
-                    <img style={{ borderRadius: "115px",width: "222px", height: "215px",marginLeft:"100px",marginTop:"22px"}}
-                        src={user.pic} alt="profile" />
+  </Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>Cras justo odio</ListGroupItem>
+    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+  </ListGroup>
+  <Card.Body>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
 
-                </div>
-                
-                    <div style={{ marginTop: '20px' }} className="col-6">
-                      
-                        <h4 style={{ fontWeight: "bold" }}> <AiFillCaretRight />  Name: {user.name} </h4><br />
-                        <h4 style={{ fontWeight: "bold" }}> <AiFillCaretRight />  Email: {user.email} </h4><br />
-                        <h4 style={{ fontWeight: "bold" }}> <AiFillCaretRight />  Phone:  {user.phone}</h4><br />
-                        <h4 style={{ fontWeight: "bold" }}> <AiFillCaretRight />  role:  {user.role}</h4>
-                      
-                    </div>
-
-              
-
-            </div>
+</div>
 
         </>
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className="row">
+<div style={{ marginLeft: "150px", fontWeight: "bold", color: "green", fontFamily: "'Shizuru', 'cursive'" }} className="heading text-center">
+    <h1>Profile Details :-</h1>
+</div><hr />
+<div className="col-2">
+
+</div>
+<div className="col-4">
+
+    <img style={{ borderRadius: "115px", width: "222px", height: "215px", marginLeft: "100px", marginTop: "22px" }}
+        src={user.pic} alt="profile" />
+
+</div>
+
+<div style={{ marginTop: '20px'}} className="col-6">
+
+    <h4 style={{ fontWeight: "bold" }}> <MdOutlineDriveFileRenameOutline style={{marginRight:"10px"}}/>  {user.name} </h4><br />
+    <h4 style={{ fontWeight: "bold" }}> <MdMarkEmailUnread  style={{marginRight:"10px"}}/> {user.email} </h4><br />
+    <h4 style={{ fontWeight: "bold" }}> <BsTelephoneForwardFill  style={{marginRight:"10px"}}/>{user.phone}</h4><br />
+    <h4 style={{ fontWeight: "bold" }}> <RiAdminFill  style={{marginRight:"10px"}}/> {user.role}</h4>
+
+</div>
+
+</div> */}
