145873f146eb0445170caebadb9d2d977d03bd81
i am sanket patel <86530664+mulanisanket0511@users.noreply.github.com>
2022-02-21 17:37:26 +0530

    emailjs

--- client/src/componets/Adminportal/Registrationform.jsx
+++ client/src/componets/Adminportal/Registrationform.jsx
@@ -10,9 +10,10 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useState } from "react";
 import axios from "axios";
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Admindetail from "./Admindetail";
-
+import emailjs from "@emailjs/browser";
+import { init } from "@emailjs/browser";
 
 
 const theme = createTheme();
@@ -26,72 +27,75 @@ export default function Signup(props) {
   const [data, setData] = useState([]);
   const [role, setrole] = React.useState();
   const [value, setValue] = React.useState("");
- const navigate = useNavigate()
+  const navigate = useNavigate();
+  React.useEffect (() => {
   if (!props.role) {
     //admin
     axios.get("http://localhost:5000/all").then((res) => {
       setData(res.data);
     });
   }
-  else {
+ 
+     else {
     //employee
-    axios.get("http://localhost:5000/employee/all",{
-      headers: {"Authorization":`Bearer ${localStorage.getItem("token")}`}
-    })
-    .then((res) => {
-      setData(res.data);
-    });
+    axios.get("http://localhost:5000/employee/all", {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+      .then((res) => {
+        setData(res.data);
+      });
   }
+}, [])
+  init("user_AWXu32CCuDFQigjASbRix");
 
   const sendData = () => {
-
-    
-
-
     console.log(data);
     if (!props.role) {
       var data = new FormData();
-    data.append("name", name);
-    data.append("email", email);
-    data.append("phone", phone);
-    data.append("password", password);
-    data.append("pic", image);
-    data.append("role", role);
+      data.append("name", name);
+      data.append("email", email);
+      data.append("phone", phone);
+      data.append("password", password);
+      data.append("pic", image);
+      data.append("role", role);
       //admin
       axios.post("http://localhost:5000/add-user", data)
         .then((res) => {
           console.log(res);
           alert("Data send successfully..");
-          navigate ("/");
+          navigate("/");
         })
         .catch((err) => console.log(err));
-    }
-    else {
+    } else {
       var data = new FormData();
-    data.append("name", name);
-    data.append("email", email);
-    data.append("phone", phone);
-    data.append("password", password);
-    data.append("employeepic", image);
-   
+      data.append("name", name);
+      data.append("email", email);
+      data.append("phone", phone);
+      data.append("password", password);
+      data.append("employeepic", image);
+      const sendemail= {
+        name: name,
+        to_email: email,
+        id: email,
+        password: password,
+      };
       //employee
-      axios.post("http://localhost:5000/employee/add-employee",data,{ 
-        headers: {Authorization:`Bearer ${localStorage.getItem("token")}`}
-    })
+      axios.post("http://localhost:5000/employee/add-employee", data, {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        })
         .then((res) => {
           console.log(res.data);
           alert("Data send successfully..");
+          emailjs.send("service_dp8vt3p","template_6auk0r2",sendemail);
           navigate("/all-employee");
         })
         .catch((err) => console.log(err));
-
     }
   };
-  
-  var emaildata = []
+
+  var emaildata = [];
   if (data === undefined) {
-    
-    data.map((item) => emaildata.push(item.email))
+    data.map((item) => emaildata.push(item.email));
   }
 
   const handleadmin = (e) => {
@@ -104,24 +108,32 @@ export default function Signup(props) {
       sendData();
     }
   };
-  const handleemplolyee = (e) =>{
+  const handleemplolyee = (e) => {
     e.preventDefault();
     if (name === "" || email === "" || phone === "" || password === "") {
       alert("please fill-up the details");
-  }
-  else{
-     sendData();
-  }
-  } 
-  
-
+    } else {
+    
+      sendData();
+    }
+  };
 
   return (
     <>
-    { props.role ? <Admindetail />:null}
-   <div className={!props.role?null:"responsive"} style={{ display: "flex", justifyContent: "center" }}>
-       
-        <div className="col-4 card" style={{ width: "450px", marginLeft: "auto !important",marginRight: "auto !important",marginTop:"50px" }}>
+      {props.role ? <Admindetail /> : null}
+      <div
+        className={!props.role ? null : "responsive"}
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <div
+          className="col-4 card"
+          style={{
+            width: "450px",
+            marginLeft: "auto !important",
+            marginRight: "auto !important",
+            marginTop: "50px",
+          }}
+        >
           <ThemeProvider theme={theme}>
             <Container component="main" maxWidth="xs">
               <CssBaseline />
@@ -137,15 +149,13 @@ export default function Signup(props) {
                 </Avatar>
 
                 <Typography
-
                   style={{ marginBottom: 20 }}
                   component="h4"
                   variant="h4"
                 >
                   {/* {/ pass the props for name /} */}
-                  
-                    {props.name}
-                              
+
+                  {props.name}
                 </Typography>
 
                 <Box noValidate sx={{ mt: 0 }}>
@@ -194,21 +204,18 @@ export default function Signup(props) {
                     autoComplete="email"
                     autoFocus
                   />
-                   {props.role ? null :
-                     
-                   
-                  <TextField
-                    margin="normal"
-                    id="outlined-select-role"
-                    required
-                    fullWidth
-                    autoFocus
-                    label="Enter Your Role"
-                    value={role}
-                    onChange={(e) => setrole(e.target.value)}
-                  >
-                  </TextField>
-              } 
+                  {props.role ? null : (
+                    <TextField
+                      margin="normal"
+                      id="outlined-select-role"
+                      required
+                      fullWidth
+                      autoFocus
+                      label="Enter Your Role"
+                      value={role}
+                      onChange={(e) => setrole(e.target.value)}
+                    ></TextField>
+                  )}
                   <TextField
                     margin="normal"
                     required
@@ -220,37 +227,33 @@ export default function Signup(props) {
                     id="password"
                     autoComplete="current-password"
                   />
-                  {!props.role ? 
-                  <Button
-                    type="submit"
-                    fullWidth
-                    onClick={(e) => handleadmin(e)}
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                  >
-                    Sign up
-                  </Button>
-                  : <Button
-                  type="submit"
-                  fullWidth
-                  onClick={(e) => handleemplolyee(e)}
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Sign up
-                </Button>
-                }
+                  {!props.role ? (
+                    <Button
+                      type="submit"
+                      fullWidth
+                      onClick={(e) => handleadmin(e)}
+                      variant="contained"
+                      sx={{ mt: 3, mb: 2 }}
+                    >
+                      Sign up
+                    </Button>
+                  ) : (
+                    <Button
+                      type="submit"
+                      fullWidth
+                      onClick={(e) => handleemplolyee(e)}
+                      variant="contained"
+                      sx={{ mt: 3, mb: 2 }}
+                    >
+                      Sign up
+                    </Button>
+                  )}
                 </Box>
               </Box>
-
             </Container>
           </ThemeProvider>
         </div>
-        </div>
-      
-
+      </div>
     </>
   );
- }
-
- 
+}
--- client/src/componets/Adminportal/Viewemployee.jsx
+++ client/src/componets/Adminportal/Viewemployee.jsx
@@ -4,12 +4,11 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Carousel } from "react-bootstrap";
-import axios, { Axios } from "axios";
+import axios from "axios";
 import { CssBaseline } from "@mui/material";
 import { useParams } from "react-router-dom";
 
 const theme = createTheme();
-
 export default function Viewemployee() {
   var { id } = useParams();
   const [employeeview, setemployeeview] = React.useState([]);
