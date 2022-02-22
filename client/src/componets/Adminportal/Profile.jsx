8d62557b667955ff6b71b4c51e9b9c745e7df68e
keyur6021 <86195043+keyur6021@users.noreply.github.com>
2022-02-22 16:56:26 +0530

    profile update

--- client/src/componets/Adminportal/Profile.jsx
+++ client/src/componets/Adminportal/Profile.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect } from "react";
-import {  Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { MdEmail } from "react-icons/md";
 import { BsTelephoneFill } from "react-icons/bs";
+import { HiUserGroup } from "react-icons/hi";
+import { FaUserCheck } from "react-icons/fa";
+import { FaMoneyBillAlt } from "react-icons/fa";
 import axios from "axios";
-import { Grid } from "@mui/material";
+import { CardContent, Grid, Typography } from "@mui/material";
 import Admindetail from "./Admindetail";
+import { typography } from "@mui/system";
 
 export const Profile = () => {
   const [user, setuser] = React.useState([]);
@@ -13,7 +17,7 @@ export const Profile = () => {
   useEffect(() => {
     axios
       .get(`http://localhost:5000/user/${id}`, {
-        headers: {"Authorization":`Bearer ${localStorage.getItem("token")}`}
+        headers: { "Authorization": `Bearer ${localStorage.getItem("token")}` }
       })
       .then((res) => {
         setuser(res.data);
@@ -24,13 +28,13 @@ export const Profile = () => {
 
   return (
     <>
-    <Admindetail />
+      <Admindetail />
       <div className="background"></div>
       <Card
         style={{
           width: "78%",
           marginLeft: "290px",
-          height: "575px",
+          height: "622px",
           marginTop: "-666px",
         }}
       >
@@ -39,10 +43,10 @@ export const Profile = () => {
             <img
               style={{
                 borderRadius: "110px",
-                width: "180px",
-                height: "175px",
-                marginLeft: "8px",
-                marginTop: "5px",
+                width: "112px",
+                height: "112px",
+                marginLeft: "6px",
+                marginTop: "4px",
               }}
               src={user.pic}
               alt="profile"
@@ -75,9 +79,79 @@ export const Profile = () => {
               />{" "}
               {user.phone}
             </div>
-            <button className="btn btn-primary">EDIT PROFILE</button>
+            <div className="edit">
+              <button className="btn btn-primary">EDIT PROFILE</button>
+            </div>
           </Grid>
-          <Grid item xs={9}></Grid>
+
+
+          <Grid item xs={1}></Grid>
+          <Grid item xs={4}>
+
+
+            <Card style={{ width: "85%", height: "210px", backgroundColor: 'rgb(165, 213, 255)' }}>
+              <CardContent>
+                <Typography sx={{ fontWeight: "bold" }} variant="h5" gutterBottom>
+                  <HiUserGroup style={{ marginBottom: "6px", marginRight: "8px" }} />
+                  Total Employee
+                </Typography>
+                <Typography>
+                  <strong style={{ fontSize: "55px" }} className="employee"> 32
+                    <sup>+</sup>
+                  </strong>
+                </Typography>
+                <button className="btn btn-dark btn-sm my-2">LEARN MORE</button>
+              </CardContent>
+            </Card>
+
+            <Card style={{ width: "85%", height: "210px", marginTop: '20px', backgroundColor: 'rgb(165, 213, 255)' }}>
+              <CardContent>
+                <Typography sx={{ fontWeight: "bold" }} variant="h5" gutterBottom>
+                 <FaMoneyBillAlt style={{ marginBottom: "6px", marginRight: "8px" }}/> Employee Salary
+                </Typography>
+                <Typography>
+                  <strong style={{ fontSize: "55px" }} className="employee"> 4.5 L
+                    <sup>+</sup>
+                  </strong>
+                </Typography>
+
+              </CardContent>
+            </Card>
+
+
+          </Grid>
+          <Grid item xs={4}>
+            <Card style={{ width: "85%", height: "210px", backgroundColor: 'rgb(165, 213, 255)' }}>
+              <CardContent>
+                <Typography sx={{ fontWeight: "bold" }} variant="h5" gutterBottom>
+                  <FaUserCheck style={{ marginBottom: "6px", marginRight: "8px" }} />
+                  Present Employee
+                </Typography>
+                <Typography>
+                  <strong style={{ fontSize: "55px" }} className="employee"> 26
+                    <sup>+</sup>
+                  </strong>
+                </Typography>
+
+              </CardContent>
+            </Card>
+
+            <Card style={{ width: "85%", height: "210px", marginTop: "20px", backgroundColor: 'rgb(165, 213, 255)' }}>
+              <CardContent>
+                <Typography sx={{ fontWeight: "bold" }} variant="h5" gutterBottom>
+                  <FaUserCheck style={{ marginBottom: "6px", marginRight: "8px" }} />
+                  Years Of Experience
+                </Typography>
+                <Typography>
+                  <strong style={{ fontSize: "55px" }} className="employee"> 10
+                    <sup>+</sup>
+                  </strong>
+                </Typography>
+
+              </CardContent>
+            </Card>
+
+          </Grid>
         </Grid>
       </Card>
     </>
